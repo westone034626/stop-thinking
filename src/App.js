0e2ea846e1114e9c9a1378d111c73a771779e413
westone034626 <dydrlaks@likelion.org>
2023-10-12 21:54:30 +0900

    feat: Wrap Main component with Layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Main from "./Main";
 import './index.css';
+import { MAX_MOBILE_WIDTH } from './constant';
+
+function Layout({ children }) {
+  const styles = {
+    container: {
+      flex: 1,
+      width: '100%',
+      maxWidth: MAX_MOBILE_WIDTH,
+      margin: '0px auto',
+    }
+  };
+  return (
+    <div style={styles.container}>{children}</div>
+  );
+}
 
 function App() {
   return (
-    <Main />
+    <Layout>
+      <Main />
+    </Layout>
   );
 }
 
